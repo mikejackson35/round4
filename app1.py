f199f23da99989a77b78f8f92080e5efb8173224
Mike_Jackson <mikejackson35@gmail.com>
2022-08-15 11:01:13 -0700

    update

--- app1.py
+++ app1.py
@@ -4,20 +4,16 @@ from dash import dcc
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv(f'https://github.com/mikejackson35/round4/blob/main/data/stats.csv')
-fig = px.bar(df[df['player_name']=='Rahm, Jon'],
-                    x = 'event_completed',
-                    y = 'sg_app')
+df = pd.read_csv(r'https://raw.githubusercontent.com/mikejackson35/round4/main/stats.csv')
+fig = px.scatter(df.loc[df.player_name=='Rahm, Jon'],
+                    x = 'sg_app',
+                    y = 'fin_text')
 
 app = dash.Dash()
-app.layout = html.Div([
-    html.Div('Hello Golf Fans'),
-    html.H1('H1 Tag Here'),
-    # html.Div(dcc.Dropdown(id='dropdown',options = [
-    #     {'label':'A', 'value':'A'},
-    #     {'label': 'B', 'value':'B'},
-    #     {'lable': 'C', 'value':'C'}])),
-    dcc.Graph(id='fig1',figure=fig)])
+app.layout = html.Div([html.Div('Hello Golf Fans'),
+             html.H1('H1 Tag Here'),
+             html.Div(dcc.Dropdown(id='dropdown',options = [{'label':'A', 'value':'A'},{'label': 'B', 'value':'B'},{'lable': 'C', 'value':'C'}])),
+             dcc.Graph(id='fig1',figure=fig)])
                                                                 
 app.run_server(debug=True)            
                                                                                                                                                                                                                          
